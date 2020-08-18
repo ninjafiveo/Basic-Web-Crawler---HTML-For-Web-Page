697ce89c528ba6ec38a3b16ad080227d063f14e5
ninjafiveo <mjsekol@live.com>
2020-08-17 21:44:18 -0400

    Add project files.

---
+++ BasicWebCrawlerWebPage/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+
+namespace BasicWebCrawlerWebPage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new WebClient();
+            var text = client.DownloadString("https://www.mahoningctc.com");
+
+            Console.WriteLine(text);
+        }
+    }
+}
